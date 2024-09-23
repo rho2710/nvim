6e05845bacc0131623dc70a64103f581f7245dd6
Robin <robin@robin.local>
2024-09-23 14:27:49 +0200

    fuck

--- init.lua
+++ init.lua
@@ -876,6 +876,15 @@ require('lazy').setup({
     }
   },
   {
+  "lervag/vimtex",
+  lazy = false,     -- we don't want to lazy load VimTeX
+  -- tag = "v2.15", -- uncomment to pin to a specific release
+  init = function()
+    -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_view_method = "zathura"
+  end
+  },
+  {
     'yacineMTB/dingllm.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
